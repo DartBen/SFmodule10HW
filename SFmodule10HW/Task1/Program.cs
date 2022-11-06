b9f1c1cf90e18c2cb19a56681c6b5bf3e85ea075
Dmitry <ryabchikk@rambler.ru>
2022-11-06 17:41:48 +0300

    task2

--- SFmodule10HW/Task1/Program.cs
+++ SFmodule10HW/Task1/Program.cs
@@ -1,7 +1,51 @@
-﻿internal class Program
+﻿using System;
+using static System.Console;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Calcutator calcutator = new Calcutator();
+        calcutator.a = calcutator.ArgumentConsoleRequest("Введите первое слогаемое");
+        calcutator.b = calcutator.ArgumentConsoleRequest("Введите второе слогаемое");
+        WriteLine($"Сумма чисел = {calcutator.Sum()}");
     }
 }
+
+interface ICalcutatorSum<in T>
+{
+    public double Sum(T a, T b);
+    public double ArgumentConsoleRequest(string a);
+    public double Sum();
+}
+
+class Calcutator : ICalcutatorSum<Double>
+{
+    public double a { get; set; }
+    public double b { get; set; }
+    public double ArgumentConsoleRequest(string a)
+    {
+        double result = 0;
+        bool parsed = false;
+        while (parsed == false)
+        {
+            try
+            {
+                WriteLine(a);
+                parsed = Double.TryParse(ReadLine(), out result);
+                if (parsed == false) throw new Exception("Вводимая строка не может быть преобразована в число!");
+                return result;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+        return result;
+    }
+    public double Sum(double a, double b)
+    {
+        return a + b;
+    }
+    public double Sum()
+    {
+        return a + b;
+    }
+}
+
--- SFmodule10HW/Task2/Program.cs
+++ SFmodule10HW/Task2/Program.cs
@@ -1,7 +1,84 @@
-﻿internal class Program
+﻿using System;
+using static System.Console;
+internal class Program
 {
+    static ILogger logger { get; set; }
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        logger = new Logger();
+        Calcutator calcutator = new Calcutator(logger);
+
+        calcutator.a = calcutator.ArgumentConsoleRequest("Введите первое слогаемое");
+        calcutator.b = calcutator.ArgumentConsoleRequest("Введите второе слогаемое");
+        WriteLine($"Сумма чисел = {calcutator.Sum()}");
+    }
+}
+
+interface ICalcutatorSum<in T>
+{
+    public double Sum(T a, T b);
+    public double ArgumentConsoleRequest(string a);
+    public double Sum();
+}
+
+class Calcutator : ICalcutatorSum<Double>
+{
+    ILogger Logger { get; }
+    public double a { get; set; }
+    public double b { get; set; }
+
+    public Calcutator(ILogger logger)
+    {
+        Logger = logger;
+    }
+    public double ArgumentConsoleRequest(string a)
+    {
+        double result = 0;
+        bool parsed = false;
+        while (parsed == false)
+        {
+            Logger.Event("Запрос аргумента");
+            try
+            {
+                WriteLine(a);
+                parsed = Double.TryParse(ReadLine(), out result);
+                if (parsed == false) throw new Exception("Вводимая строка не может быть преобразована в число!");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+                WriteLine(ex.Message);
+            }
+        }
+        Logger.Event("Аргумент был записан");
+        return result;
+    }
+    public double Sum(double a, double b)
+    {
+        Logger.Event("Запрос суммы аргументов");
+        return a + b;
+    }
+    public double Sum()
+    {
+        return Sum(a, b);
+    }
+}
+
+interface ILogger
+{
+    void Event(string message);
+    void Error(string message);
+}
+
+class Logger : ILogger
+{
+    public void Error(string message)
+    {
+        WriteLine($"Logger: {message}");
+    }
+
+    public void Event(string message)
+    {
+        WriteLine($"Logger: {message}");
     }
 }
